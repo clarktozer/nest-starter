78b989f9ceb4a5db382f6cbbd49f42ff9e12cd64
Clark Tozer <clarkwtozer@gmail.com>
2021-07-26 11:28:11 +1000

    validation

--- src/user/dto/createUserDto.ts
+++ src/user/dto/createUserDto.ts
@@ -1,15 +1,20 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsString()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsString()
   avatar: string;
 
+  @IsString()
   googleId?: string;
 
+  @IsString()
   facebookId?: string;
 }
--- src/user/dto/registerUserDto.ts
+++ src/user/dto/registerUserDto.ts
@@ -1,13 +1,16 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @IsEmail()
+  @IsString()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsNotEmpty()
   @MinLength(8)
+  @IsString()
   password: string;
 }
